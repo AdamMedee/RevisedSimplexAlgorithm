636ed20e6ef8a0d71aa6d7904d71ba5b511df609
Adam Mehdi <adammehdi98765@gmail.com>
2021-04-11 20:38:18 -0400

    Add files via upload

---
+++ Revised Simplex Algorithm Project - Copy/Assets/Scripts/Camera3dcontrol.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.U2D;
+using Quaternion = System.Numerics.Quaternion;
+
+public class Camera3dcontrol : MonoBehaviour
+{
+    private Camera cam;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = gameObject.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}
---
+++ Revised Simplex Algorithm Project - Copy/Assets/Scripts/MainButtonFuncs.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class MainButtonFuncs : MonoBehaviour
+{
+    public GameObject pythonpathinp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void SetPythonPath()
+    {
+        string txtPath = "Assets/Scripts/python-path.txt";
+        using (FileStream fs = File.Create(txtPath)) { }
+        StreamWriter writer = new StreamWriter(txtPath, true);
+        string newpath = pythonpathinp.GetComponent<InputField>().text;
+        writer.WriteLine(@newpath);
+        writer.Close();
+    }
+}
